aac2a76efda3cbb74fd3276221055c1630389489
alpay.basol <alpay41@gmail.com>
2023-03-26 18:17:33 +0300

    Feed add user İnputs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hesap Makinesi");
+
+            Console.WriteLine("Birinci Sayıyı Giriniz  ");
+            int sayi1 = int.Parse(Console.ReadLine()); 
+            
+            Console.WriteLine("İkinci Sayıyı Giriniz  ");
+            int sayi = int.Parse(Console.ReadLine());
         }
     }
 }
