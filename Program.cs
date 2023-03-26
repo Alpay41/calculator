9ad06fc55a9ab2a3ad1e8630d96f51f252a014cf
alpay.basol <alpay41@gmail.com>
2023-03-26 18:34:10 +0300

    fix :add sum funtion

--- Program.cs
+++ Program.cs
@@ -9,14 +9,19 @@
             Console.WriteLine("Birinci Sayıyı Giriniz  ");
             int sayi1 = int.Parse(Console.ReadLine()); 
             
-            Console.WriteLine("İşlem Seçiniz   (+) (-) (*) (/) ");
+            Console.WriteLine("İşlem Seçiniz   (+) (-) (*) (/) (%)");
             string islem = (Console.ReadLine()); 
             
             Console.WriteLine("İkinci Sayıyı Giriniz  ");
             int sayi = int.Parse(Console.ReadLine());
 
 
-
+            switch ("islem")
+            {
+                case "+":
+                    Console.WriteLine(sayi1+sayi);
+                    break;
+            }
 
         }
     }
